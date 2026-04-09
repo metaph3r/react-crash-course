5f0443159aab617a50d4e0e59073edc48e9d9c17
Silvio Glöckner <silvio.gloeckner@adesso.de>
2026-04-09 13:10:40 +0200

    added spring backend

---
+++ backend/src/main/java/de/adesso/react_crash_course/ReactCrashCourseApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.react_crash_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactCrashCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactCrashCourseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/adesso/react_crash_course/ReactCrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.react_crash_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactCrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
